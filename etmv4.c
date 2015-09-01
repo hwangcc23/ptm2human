bf651d65a50b7f15f18d67eb767a24cd611641a3
hwangcc23 <hwangcc@csie.nctu.edu.tw>
2015-09-01 22:54:18 +0800

    Add decoding for trace info packets

--- etmv4.c
+++ etmv4.c
@@ -12,9 +12,7 @@ DEF_TRACEPKT(trace_info, 0xff, 0x01);
 
 DECL_DECODE_FN(extension)
 {
-    int index, cnt;
-
-    index = 1;
+    int index = 1, cnt;
 
     switch (pkt[index]) {
     case 0:
@@ -31,9 +29,6 @@ DECL_DECODE_FN(extension)
             return -1;
         }
         LOGD("[async]\n");
-
-        /* SYNCING -> INSYNC */
-        stream->state++;
 
         break;
 
@@ -61,9 +56,101 @@ DECL_DECODE_FN(extension)
     return index;
 }
 
+/* FIXME: handle not only the instruction trace stream but also the data trace stream */
 DECL_DECODE_FN(trace_info)
 {
-    return -1;
+    int index = 1, i;
+    unsigned int plctl = 0, info = 0, key = 0, spec = 0, cyct = 0;
+    const unsigned char c_bit = 0x80;
+    unsigned char data;
+
+    for (i = 0; i < 4; i++) {
+        data = pkt[index++];
+        plctl |= (data & ~c_bit) << (7 * i);
+        if (!(data & c_bit)) {
+            break;
+        }
+    }
+    if (i >= 1) {
+        /* ETMv4 arch spec 6/228: A trace unit must not output more than 1 PLCTL field in a Trace info packet */
+        LOGE("More than 1 PLCTL field in the trace info packet\n");
+        return -1;
+    } else {
+        LOGD("[traceinfo] plctl = 0x%X\n", plctl);
+    }
+
+    if (plctl & 1) {
+        /* the INFO section is present*/
+        for (i = 0; i < 4; i++) {
+            data = pkt[index++];
+            info |= (data & ~c_bit) << (7 * i);
+            if (!(data & c_bit)) {
+                break;
+            }
+        }
+        if (i >= 1) {
+            /* ETMv4 arch spec 6/228: A trace unit must not output more than 1 INFO field in a Trace info packet */
+            LOGE("More than 1 INFO field in the trace info packet\n");
+            return -1;
+        } else {
+            LOGD("[traceinfo] info = 0x%X\n", info);
+        }
+    }
+
+    if (plctl & 2) {
+        /* the KEY section is present*/
+        for (i = 0; i < 4; i++) {
+            data = pkt[index++];
+            key |= (data & ~c_bit) << (7 * i);
+            if (!(data & c_bit)) {
+                break;
+            }
+        }
+        if (i >= 4) {
+            /* 4 fileds are enough since p0_key_max is a 32-bit integer */
+            LOGE("More than 4 KEY fields in the trace info packet\n");
+            return -1;
+        } else {
+            LOGD("[traceinfo] key = 0x%X\n", key);
+        }
+    }
+
+    if (plctl & 4) {
+        /* the SPEC section is present*/
+        for (i = 0; i < 4; i++) {
+            data = pkt[index++];
+            spec |= (data & ~c_bit) << (7 * i);
+            if (!(data & c_bit)) {
+                break;
+            }
+        }
+        if (i >= 4) {
+            /* 4 fileds are enough since max_spec_depth is a 32-bit integer */
+            LOGE("More than 4 SPEC fields in the trace info packet\n");
+            return -1;
+        } else {
+            LOGD("[traceinfo] curr_spec_depth = 0x%X\n", spec);
+        }
+    }
+
+    if (plctl & 8) {
+        /* the CYCT section is present*/
+        for (i = 0; i < 2; i++) {
+            data = pkt[index++];
+            cyct |= (data & ~c_bit) << (7 * i);
+            if (!(data & c_bit)) {
+                break;
+            }
+        }
+        if (i >= 2) {
+            LOGE("More than 2 CYCT fields in the trace info packet\n");
+            return -1;
+        } else {
+            LOGD("[traceinfo] cc_thresold = 0x%X\n", cyct);
+        }
+    }
+
+    return index;
 }
 
 struct tracepkt *etmv4pkts[] =
@@ -84,8 +171,16 @@ int synchronization(struct stream *stream)
         c = stream->buff[i];
         if ((c & PKT_NAME(extension).mask) == PKT_NAME(extension).val) {
             p = DECODE_FUNC_NAME(extension)((const unsigned char *)&(stream->buff[i]), stream);
-            if ((p > 0) && (stream->state >= INSYNC)) {
-                return i;
+            if (p != 12)
+                continue;
+            c = stream->buff[i + 12];
+            if ((c & PKT_NAME(trace_info).mask) == PKT_NAME(trace_info).val) {
+                p = DECODE_FUNC_NAME(trace_info)((const unsigned char *)&(stream->buff[i + 12]), stream);
+                if (p > 0) {
+                    /* SYNCING -> INSYNC */
+                    stream->state++;
+                    return i;
+                }
             }
         }
     }
